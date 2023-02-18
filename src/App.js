4f8857527441e0d7651ebb989588b6a27122e348
RahulG5894 <45035258+RahulG5894@users.noreply.github.com>
2023-02-19 01:05:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Game from "./component/Game";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>TicTacToe</h1>
+      <Game />
+    </div>
+  );
+}
